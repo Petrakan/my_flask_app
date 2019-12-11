ab80f7cbce801b8752e3a1bdd054acd3be76ee3a
Petrakan <petrakan.code@gmail.com>
2019-12-12 00:22:11 +0300

    init

--- app/app.py
+++ app/app.py
@@ -1,72 +1,84 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
-from config import Configuration
-
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
-
-
-
 app = Flask(__name__)
-app.config.from_object(Configuration)
-
-
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://admin:1234@localhost/flask_app'
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return "User('{}', '{}', '{}')".format(self.username, self.email, self.image_file)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return "User('{}', '{}')".format(self.title, self.date_posted)
 
 
 posts = [
     {
-        'author':'Alexey Petrakov',
-        'title':'Blog Post 1',
-        'content':'First post content',
-        'date_posted':'December 11, 2019 ',
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
     },
     {
-        'author':'Galina Petrakova',
-        'title':'Blog Post 2',
-        'content':'Second post content',
-        'date_posted':'November 25, 2019 ',
-    },
-    {
-        'author':'Viktoria Spirina',
-        'title':'Blog Post 3',
-        'content':'Third post content',
-        'date_posted':'August 15, 2019 ',
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
     }
 ]
 
 
-@app.route('/')
-def home_page():
-    return render_template('home_page.html', posts=posts)
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
 
 
-@app.route('/about')
+@app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash('Аккаунт создан для {}!'.format(form.username.data), 'success')
-        return redirect(url_for('home_page'))
-    return render_template('register.html', title ='Register', form=form)
+        flash('Account created for {}!'.format(form.username.data), 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html', title ='Login', form=form)
-
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- app/forms.py
+++ app/forms.py
@@ -3,30 +3,20 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
-
 class RegistrationForm(FlaskForm):
-    username = StringField('Логин',
-                            validators = [DataRequired(), Length(min=2, max=25)])
-
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
-                            validators = [DataRequired(), Email()])
-
-    password =  PasswordField('Пароль', 
-                            validators = [DataRequired()])
-
-    confirm_password =  PasswordField('Повторите пароль', 
-                            validators = [DataRequired(), EqualTo(password)])
-    
-    submit = SubmitField('Зарегистрироваться')
-
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
-                            validators = [DataRequired(), Email()])
-
-    password =  PasswordField('Пароль', 
-                            validators = [DataRequired()])
-    remember = BooleanField('Запомнить меня')
-    
-    submit = SubmitField('Войти')
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
